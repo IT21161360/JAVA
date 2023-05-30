1bcf7a097329f9899e61f9a610fc7a3a008201e9
IT21161360 <it21161630@my.sliit.lk>
2023-05-30 21:12:02 +0530

    commit

---
+++ INSERTSORT/InsertSort.java
@@ -0,0 +1,27 @@
+package INSERTSORT;
+
+public class InsertSort {
+    public static void insertSort(int array[]){
+         for(int counter=1;counter<array.length;counter++){
+            int key = array[counter];
+            int i = counter-1;
+            while(i>=0 && array[i]>key){
+                array[i+1] = array[i];
+                i = i-1;
+            }
+            array[i+1] = key;
+         }
+    }
+
+    public static void printArray(int array[]){
+        for(int counter=0;counter<array.length;counter++){
+            System.out.println(array[counter]);
+        }
+    }
+
+    public static void main(String[] args){
+        int array[] = {9,8,7,6,5,4,3,2};
+        insertSort(array);
+        printArray(array);
+    }
+}
